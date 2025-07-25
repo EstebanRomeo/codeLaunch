00deba2c892a9933129f4fa1c3e3b60cdef2959a
EstebanRomeo <estebannicolasromeo@gmail.com>
2025-07-25 12:39:22 -0300

    Vercel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './Inicio';
 import Formulario from './Formulario';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <Router>
+      <SpeedInsights/>
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/formulario" element={<Formulario />} />
