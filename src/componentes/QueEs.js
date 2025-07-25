45a7c8d4878ec82dae2702d09cc422e07b639c19
EstebanRomeo <estebannicolasromeo@gmail.com>
2025-07-25 12:30:06 -0300

    Cambio nombre subtitulo

--- src/componentes/QueEs.js
+++ src/componentes/QueEs.js
@@ -5,7 +5,7 @@ const QueEs = () => {
   return (
     <div className='containerQueEs'>
         <div className='containerInfoTrata'>
-            <h2>¿De que trata el proyecto?</h2>
+            <h2>¿De que consta esta iniciativa?</h2>
             <p>En muchas ciudades, hay talento pero no oportunidades. Este proyecto nace para unir programadores, diseñadores y entusiastas tech <mark>sin experiencia</mark>, que quieran trabajar en proyectos reales, ganar práctica en equipo y preparar un portfolio que abra puertas a trabajos mejores. El objetivo es que esta comunidad sea un trampolín para quienes quieren dar su <mark>primer salto profesional</mark>.</p>
         </div>
     </div>
